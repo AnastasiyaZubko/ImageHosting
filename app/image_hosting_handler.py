a5d38e35555b32c1569e20dac1c35edc24410324
Anastasiya <nastya.suhodolova@mail.ru>
2026-05-24 19:27:30 +0300

    fix: fix nginx.conf

--- app/image_hosting_handler.py
+++ app/image_hosting_handler.py
@@ -25,8 +25,8 @@ class ImageHostingHandler(BaseHandler):
         elif self.path == '/images':
             self.template_response('images.html')
 
-        elif any((self.path.endswith(ext) for ext in ['.css', '.js', '.png'])):
-            self.send_static_file(self.path)
+        # elif any((self.path.endswith(ext) for ext in ['.css', '.js', '.png'])):
+        #     self.send_static_file(self.path)
         else:
             self.html_response('Not Found', 404)
 
