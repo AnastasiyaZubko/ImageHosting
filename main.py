867e2736f5107f8adcc373450c32e7b591f6c742
Anastasiya <nastya.suhodolova@mail.ru>
2026-05-24 19:42:56 +0300

    fix: restart app on failure

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app import settings
 from app.settings import LOG_PATH
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    format='[%(asctime)s] - %(name)s - %(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     handlers=[
                         logging.StreamHandler(),
